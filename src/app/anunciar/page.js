9c0a34e657213af333bde6f948859f88cb72c8c5
KaioGroot <85202894+KaioGroot@users.noreply.github.com>
2025-03-09 22:44:29 -0400

    Update page.js
    bom, falha no envio do anúncio feita.

--- src/app/anunciar/page.js
+++ src/app/anunciar/page.js
@@ -39,7 +39,7 @@ export default function Anunciar() {
     formData.append('photo', pet.photo);
 
     try {
-      const response = await fetch('http://localhost:3000/api/anuncio/route', {
+      const response = await fetch('/api/anuncio/route', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
